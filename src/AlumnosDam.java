bfc2ac17af588f70b91efa1660c466a2380dcf2d
BELEN <BELEN@DESKTOP-GV24251>
2025-05-05 20:25:14 +0200

    tablas de multiplicar

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -3,6 +3,12 @@ public class AlumnosDam {
 
 	public static void main(String[] args) {
 		System.out.println("Alumnos Dam");
-	}
-
-}
+		
+		for(int i=0; i<=10; i++) {
+			System.out.println("Tabla del " + i + ":");
+		}
+		for(int j=0; j<=10; i++) {
+			System.out.println(i + "x" + j + "=" + (i+j));
+		}
+	}//fin del main
+}//fin de la clase
