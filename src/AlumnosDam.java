fb8d22c0fe569c99cda9204d68ff7b852dd32fdc
Ivan <Ivan@RUBEN>
2025-05-05 11:07:08 +0200

    Modificado por Adrián González

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -1,8 +1,12 @@
-
 public class AlumnosDam {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Alumnos Dam");
+		
+		System.out.println("Alumno: Adrián González Baamonde");
 	}
 
 }
+
+
