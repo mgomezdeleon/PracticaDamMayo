b21d9a7ea522827c0db9b11461f16e3bb2128b8e
Marina Torre Duran <Marina Torre Duran@LAPTOP-7AN9ONCM>
2025-05-06 08:43:45 +0200

    Tablas de multiplicar creadas por Marina

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -3,6 +3,25 @@ public class AlumnosDam {
 
 	public static void main(String[] args) {
 		System.out.println("Alumnos Dam");
-	}
+		
+		imprimirTablas();
+	
+	}//fin main
 
-}
+	public static void imprimirTablas() {
+		
+		for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabla de multiplicar del " + i + ":");
+            
+            for (int j = 1; j <= 10; j++) {
+                int resultado = i * j;
+                System.out.println(i + " x " + j + " = " + resultado);
+            }
+        
+        System.out.println(); // Línea en blanco para separar las tablas
+
+		}
+	
+	}//fin imprimirTablas
+	
+}//fin clase
