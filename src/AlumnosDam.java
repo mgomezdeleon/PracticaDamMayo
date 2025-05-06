c1207ff5fa6c2588007e9eca4ae5842a1cf103a2
Gerardo <Gerardo@Gerardo>
2025-05-07 01:30:22 +0200

    Rama

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -2,7 +2,13 @@
 public class AlumnosDam {
 
 	public static void main(String[] args) {
-		System.out.println("Alumnos Dam");
-	}
-
-}
+		
+		        for (int i = 1; i <= 10; i++) {
+		          
+		            for (int j = 1; j <= 10; j++) {
+		                System.out.println(i + " x " + j + " = " + (i * j));
+		            }
+		       
+		        }
+		    }
+		}
