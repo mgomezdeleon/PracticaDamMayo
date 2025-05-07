d32859022ac888ef7577ed46f4d781617850c42a
Victor <ALUMNOS@P624>
2025-05-07 11:59:26 +0200

    Tablas 10

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -3,6 +3,20 @@ public class AlumnosDam {
 
 	public static void main(String[] args) {
 		System.out.println("Alumnos Dam");
+	
+	    imprimirTablas();
 	}
-
+	
+	public static void imprimirTablas() {
+		//1 a 10 para imprimir las tablas
+	    for (int i = 1; i <= 10; i++) {
+	        System.out.println("Tabla del " + i + ":");
+	        // Imprimimos
+	        for (int j = 1; j <= 10; j++) {
+	            System.out.println(i + " x " + j + " = " + (i * j));
+	        }
+	        // Línea en blanco
+	        System.out.println();
+	    }
+	}
 }
