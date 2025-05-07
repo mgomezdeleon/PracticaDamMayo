0ebf383dd0f05a281a5d530a0c2e2d76a0dc40a5
dpaarg <dpaarg@ceinmark.net>
2025-05-07 22:07:14 +0200

    Update AlumnosDam.java

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -3,6 +3,16 @@ public class AlumnosDam {
 
 	public static void main(String[] args) {
 		System.out.println("Alumnos Dam");
+		imprimirTablasMultiplicar();
+	}
+	public static void imprimirTablasMultiplicar() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Tabla del " + i + ":");
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + " x " + j + " = " + (i * j));
+			}
+			System.out.println(); // Línea en blanco para separar cada tabla
+		}
 	}
 
 }
