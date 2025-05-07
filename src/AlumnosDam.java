0b00b31d39185fbf2d64bbe9936140c2fbe510a0
Ignacio Gonzalez <igonzalezs@ceinmark.net>
2025-05-07 13:17:22 +0200

    Feat: Added Tablas de Multiplicar

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -2,7 +2,14 @@
 public class AlumnosDam {
 
 	public static void main(String[] args) {
-		System.out.println("Alumnos Dam");
+		int tempResult = 0;
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				tempResult = i * j;
+				System.out.println(i + " * " + j + " = " + tempResult);
+			}
+			System.out.println("----------------------");
+		}
 	}
 
 }
