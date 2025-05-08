80261503606ce42e57b76e6fb7745d31347cd15d
cuchi <cuchi@LAPTOP-B1JK6E79>
2025-05-08 12:37:40 +0200

    rama_1

--- src/AlumnosDam.java
+++ src/AlumnosDam.java
@@ -1,8 +1,13 @@
-
 public class AlumnosDam {
-
 	public static void main(String[] args) {
 		System.out.println("Alumnos Dam");
-	}
-
+        // Imprime las 10 tablas de multiplicar directamente en el main
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabla del " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println(); // Salto de línea entre tablas
+        }
+    }
 }
---
+++ src/Paketon/Repaso_alejandro.java
@@ -0,0 +1,9 @@
+package Paketon;
+
+public class Repaso_alejandro {
+	public static void main (String[]args){
+		int suma=2+2;
+		System.out.println("hola cro, suma sencilla: 2+2 = "+suma);
+	}
+
+}
