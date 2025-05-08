7371b9668b5ad79b668d935522dac20bf02cb307
A <A@HP-ProBookG5>
2025-05-08 16:35:02 +0200

    Añadida función para imprimir tablas de multiplicar

---
+++ src/TablasMultiplicar.java
@@ -0,0 +1,16 @@
+public class TablasMultiplicar {
+
+    public static void main(String[] args) {
+        imprimirTablasMultiplicar();
+    }
+
+    public static void imprimirTablasMultiplicar() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabla del " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println(); 
+        }
+    }
+}
